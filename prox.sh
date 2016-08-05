17b4df93e6906d68e5224a36633481f1b6afa99b
Borislav Todorov <netacc@gmail.com>
2016-08-05 14:22:50 +0300

    Add files via upload

---
+++ prox.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ "`echo 'PUT YOUR PASSWORD HERE' | sudo -S networksetup -getsocksfirewallproxy Wi-Fi | grep '^Enabled'`" = 'Enabled: Yes' ]; then
+    sudo networksetup -setsocksfirewallproxystate Wi-Fi off
+    echo 'Turned off!'
+else
+    sudo networksetup -setsocksfirewallproxystate Wi-Fi on
+    echo 'Turned on!'
+fi
+
+wait
+tor ExitNodes {us}
+
+echo ''
+echo 'SOCKS Proxy'
+networksetup -getsocksfirewallproxy Wi-Fi | grep '^Enabled'
+echo ''
+networksetup -getsocksfirewallproxy Wi-Fi | grep '^Server'
+
+networksetup -getsocksfirewallproxy Wi-Fi | grep '^Port'
+echo ''
+networksetup -getmacaddress wi-fi
+networksetup -getproxyautodiscovery wi-fi
+# networksetup -setpassiveftp wi-fi off
+echo ''
+networksetup -getpassiveftp wi-fi
+
+networksetup -getinfo wi-fi
+echo ''
+echo 'Conputer Name'
+networksetup -getcomputername wi-fi
+echo ''
+echo 'DNS Server'
+networksetup -getdnsservers wi-fi
+echo ''
+echo 'Web Proxy'
+networksetup -getwebproxy wi-fi
+
+echo ''
